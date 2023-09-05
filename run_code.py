a66219b0cbb5ee77cdd87d345c4ee24f3bea9272
Klickeazy <karthikpar22@gmail.com>
2023-09-05 08:45:01 -0500

    File cleanup + Test dump

--- run_code.py
+++ run_code.py
@@ -5,11 +5,11 @@ if __name__ == "__main__":
 
     # exp_master_list = [22, 41, 19, 20, 34, 31, 47, 49, 50, 51]
 
-    exp_list = [47, 49, 34]
+    exp_list = [22, 41, 19, 20, 34, 31, 47, 49, 50, 51]
 
     for exp_no in exp_list:
 
-        ff.simulate_experiment(exp_no)
+        # ff.simulate_experiment(exp_no)
 
         ff.plot_experiment(exp_no)
 
