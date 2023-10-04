a4768471990ab9765688ae5d8baf68a0e84f10f5
Klickeazy <karthikpar22@gmail.com>
2023-10-04 10:21:39 -0500

    Image + Code update

--- run_code.py
+++ run_code.py
@@ -17,11 +17,10 @@ if __name__ == "__main__":
     print('Code run start')
 
     # exp_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    # exp_list = [1, 9]
-    exp_list = [11]
+    exp_list = [1, 9]
 
-    run_flag = True
-    # run_flag = False
+    # run_flag = True
+    run_flag = False
 
     plot_flag = True
     # plot_flag = False
